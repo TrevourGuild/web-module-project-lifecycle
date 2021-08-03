e809425140c6d5f9fc429a701f004e1ef86d2be6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-03 13:02:05 -0400

    lecture work-in-progress

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -24,9 +24,9 @@ class App extends React.Component {
     console.log("App: Renders Component");
     return (
       <div className="App">
-        {
-          (this.state.pokemon.length > 0) ? <Pokemon pokemon={this.state.pokemon} /> : <div>Loading</div>
-        }
+        
+          <Pokemon pokemon={this.?state.pokemon} /> 
+        
       </div>
     );
   }
