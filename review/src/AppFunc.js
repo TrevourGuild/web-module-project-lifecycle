074d7abb0d725f210f9debb459ab7eb0dba25332
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-03 12:16:51 -0400

    lecture work-in-progress

--- review/src/AppFunc.js
+++ review/src/AppFunc.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 const AppFunc = () => {
+  console.log("AppFunc: Setup State");
   const [name, setName] = useState("Warren");
   
   const handleClick = () => {
+    console.log("AppFunc: Change Our State");
     setName("Allison");
   };
 
+  console.log("AppFunc: Render DOM");
   return (
     <div>
       <h1>Hello {name}!</h1>
