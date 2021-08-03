02b93cb252797f95ae733670f6c9148f8bb11213
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-03 13:16:03 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-    
+    render() {
+        return(<div>
+            <h1>Dog Picture Getting System 1.1</h1>
+        </div>);
+    }
 }
 
 export default App;
