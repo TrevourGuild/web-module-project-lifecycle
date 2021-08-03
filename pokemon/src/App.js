2d6899c2f3ebc753f4434d855485ee632a103b3e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-03 12:48:08 -0400

    lecture work-in-progress

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -6,14 +6,16 @@ import './styles.scss';
 import Pokemon from './components/Pokemon';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      pokemon: data
-    };
+  state = {
+    pokemon: data
+  }
+
+  componentDidMount() {
+    console.log("App: Component Mounts");
   }
 
   render() {
+    console.log("App: Renders Component");
     return (
       <div className="App">
         <Pokemon pokemon={this.state.pokemon} />
