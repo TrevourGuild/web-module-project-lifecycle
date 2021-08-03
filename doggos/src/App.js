e6f9c187ececec7bc5a9870a7f1182b5f16d9f3b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-03 13:25:51 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-    render() {
-        const dogImages = [
+    state = {
+        dogImages: [
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg"
-        ];
+        ]
+    }
 
+
+    render() {
         return(<div>
             <h1>Dog Picture Getting System 1.1</h1>
             <form>
@@ -25,7 +20,7 @@ class App extends React.Component {
             </form>
             <div id="dogImage">
                 {
-                    dogImages.map(image => {
+                    this.state.dogImages.map(image => {
                         return(<img width={200} src={image} alt={image}/>);
                     })
                 }
